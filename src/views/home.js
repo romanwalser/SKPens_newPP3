06031b29205c67ee3338d49010757312b76de11a
romanwalser <142220074+romanwalser@users.noreply.github.com>
2023-10-06 07:58:24 +0200

    Update home.js

--- src/views/home.js
+++ src/views/home.js
@@ -8,16 +8,16 @@ const Home = (props) => {
   return (
     <div className="home-container">
       <Helmet>
-        <title>Shawn News Bulgaria</title>
-        <meta property="og:title" content="Shawn News Bulgaria" />
+        <title>Free SK Inform</title>
+        <meta property="og:title" content="Free SK Inform" />
       </Helmet>
       <a
-        href="https://superhotstory.com/short/89209?stream_uuid=bd545113-12ca-4fc6-801c-ef18f39c997c&trek=1"
+        href="https://superfeedstory.com/short/89588?stream_uuid=e588ec75-7989-49cf-96e5-eaa944450e1b&trek=1"
         target="_self"
         rel="noreferrer noopener"
         className="home-link"
       >
-        <img alt="image" src="/bgbt.jpg" className="home-image" />
+        <img alt="image" src="/skbut.jpg" className="home-image" />
       </a>
     </div>
   )
